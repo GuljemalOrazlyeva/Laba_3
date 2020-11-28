49e7f52c1fc8e42102ab0801e43debd870443385
GuljemalOrazlyeva <gul2000jemal@yandex.ru>
2020-11-28 15:06:33 +0300

    Added:class fields

--- src/bsu/rfct/orazlyeva/lab_3/GornerTableCellRenderer.java
+++ src/bsu/rfct/orazlyeva/lab_3/GornerTableCellRenderer.java
@@ -29,3 +29,46 @@ public class GornerTableCellRenderer
         }
         return false;
     }
+    public GornerTableCellRenderer() {
+// Показывать только 5 знаков после запятой
+        formatter.setMaximumFractionDigits(5);
+// Не использовать группировку (т.е. не отделять тысячи
+// ни запятыми, ни пробелами), т.е. показывать число как "1000",
+// а не "1 000" или "1,000"
+        formatter.setGroupingUsed(false);
+// Установить в качестве разделителя дробной части точку, а не
+// запятую. По умолчанию, в региональных настройках
+// Россия/Беларусь дробная часть отделяется запятой
+        DecimalFormatSymbols dottedDouble =
+                formatter.getDecimalFormatSymbols();
+        dottedDouble.setDecimalSeparator('.');
+        formatter.setDecimalFormatSymbols(dottedDouble);
+// Разместить надпись внутри панели
+        panel.add(label);
+// Установить выравнивание надписи по левому краю панели
+        panel.setLayout(new FlowLayout(FlowLayout.LEFT));
+    }
+    public Component getTableCellRendererComponent(JTable table,
+                                                   Object value, boolean isSelected, boolean hasFocus, int row, int col) {
+// Преобразовать type в строку с помощью форматировщика
+        String formattedType = formatter.format(value);
+// Установить текст надписи равным строковому представлению числа
+        label.setText(formattedType);
+        if (col==1 && needle!=null && needle.equals(formattedType)) {
+// Номер столбца = 1 (т.е. второй столбец) + иголка не null
+// (значит что-то ищем) +
+// значение иголки совпадает со значением ячейки таблицы -
+// окрасить задний фон панели в красный цвет
+            panel.setBackground(Color.RED);
+        } else if (col==1 && isCoincideFractionalAndIntegerPartsOfNumber(formattedType)) {
+            panel.setBackground(Color.GREEN);
+        } else {
+// Иначе - в обычный белый
+            panel.setBackground(Color.WHITE);
+        }
+        return panel;
+    }
+    public void setNeedle(String needle) {
+        this.needle = needle;
+    }
+}
